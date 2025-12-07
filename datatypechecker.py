f42273bd88eb3947ee76668c843633975b462d2e
Muhammad Wali Raza <waleeraxa@gmail.com>
2025-12-07 16:03:17 +0500

    Add datatype checker for a variable
    Program to check if a variable is of a specific data type.

---
+++ datatypechecker.py
@@ -0,0 +1,10 @@
+age=True
+
+if type(age) == int:
+    print("Age is an integer")
+elif type(age) == float:
+    print("Age is a float")
+elif type(age) == str:
+    print("Age is a string")
+elif type(age) == bool:
+    print("Age is a boolean")
