c12d57681c4582938211a193a6f14e82b99d3f6e
Muhammad Wali Raza <waleeraxa@gmail.com>
2026-02-03 14:00:42 +0500

    fix: clean the calculator v2 program

--- 02-control-flow/calculator_v2.py
+++ 02-control-flow/calculator_v2.py
@@ -1,23 +1,23 @@
-num1=input("Enter a number: ")
-num2=input("Enter another number: ")
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
 print("Calculator: \n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division")
 choice = input("Select option (1-4): ")
 
 if choice == '1':
-    sum = float(num1) + float(num2)
+    sum = num1 + num2
     print("The sum of", num1, "and", num2, "is", sum)
 
 elif choice == '2':
-    diff = float(num1) - float(num2)
+    diff = num1 - num2
     print("The difference between", num1, "and", num2, "is", diff)
 
 elif choice == '3':
-    prod = float(num1) * float(num2)
+    prod = num1 * num2
     print("The product of", num1, "and", num2, "is", prod)
 
 elif choice == '4':
-    if float(num2) != 0:
-        quot = float(num1) / float(num2)
+    if num2 != 0:
+        quot = num1 / num2
         print("The division of", num1, "by", num2, "is", quot)
     else:
         print("Math Error")
