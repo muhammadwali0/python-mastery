2226f887112adb056e8da671507dc8c680a1e0b8
Muhammad Wali Raza <waleeraxa@gmail.com>
2026-02-06 22:39:11 +0500

    feat: add a program to iplement slicing logic

---
+++ 03-inbuilt-data-structures/slicing.py
@@ -0,0 +1,7 @@
+## Program to print first and last five elements and also elements from index 5 to 15
+
+integers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+print("First five elements:", integers[0:5])
+print("Last  five elements:", integers[-1:-6:-1])
+print("Elements of index 5 to 15:", integers[5:16])
