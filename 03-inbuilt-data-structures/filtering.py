5b9be556e5714acc2fc693467055ffc473f17918
Muhammad Wali Raza <waleeraxa@gmail.com>
2026-02-08 01:26:25 +0500

    feat: add a program to implement list filtering logic

---
+++ 03-inbuilt-data-structures/filtering.py
@@ -0,0 +1,7 @@
+## Program to print only even numbers from the list into a new list
+
+list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+newList = [i for i in list if i % 2 == 0]
+
+print(newList)
