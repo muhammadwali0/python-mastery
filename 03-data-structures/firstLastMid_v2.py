b42f60be499beffafcbc6005651b49f040482ab8
muhammadwali0 <waleeraxa@gmail.com>
2026-02-10 23:49:21 +0500

    feat: add a program to access tuple elements

---
+++ 03-data-structures/firstLastMid_v2.py
@@ -0,0 +1,8 @@
+## Program to print first, last and middle element of a tuple
+
+tpl = tuple(range(1,11))
+
+print("First element of the tuple is:", tpl[0])
+print("Last element of the tuple is:", tpl[-1])
+print("Middle elements of the tuple are:", tpl[4:6])
+
