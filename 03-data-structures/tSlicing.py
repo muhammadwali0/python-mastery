d0ca2dbfbf267fa383832c73e95d590961e473cd
muhammadwali0 <waleeraxa@gmail.com>
2026-02-10 23:58:57 +0500

    feat: add a program to implement tuple slicing

---
+++ 03-data-structures/tSlicing.py
@@ -0,0 +1,9 @@
+## Program to print first and last three elements as well as elements from index 2 to 5 of a tuple
+
+num = tuple(range(1,11))
+
+print("First three elements of the tuple:", num[0:3])
+print("Last three elements of the tuple:", num[7::])
+print("Elements from index 2 to 5 of the tuple:", num[2:5])
+
+
