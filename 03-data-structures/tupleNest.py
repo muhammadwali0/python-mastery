54e128ce13d7e613eb5a69c4d2bbbf4feb1a3d47
muhammadwali0 <waleeraxa@gmail.com>
2026-02-12 01:17:12 +0500

    feat: add a program to implement iteration on nested tuples

--- 03-data-structures/tupleNest.py
+++ 03-data-structures/tupleNest.py
@@ -1,4 +1,4 @@
-## Program to create a nested tuple and pritn each element
+## Program to create a nested tuple and print each element
 
 tpl = ((1, 2, 3, 4), (5, 6, 7, 8), (9, 10, 11, 12), (13, 14, 15, 16))
 
