3e28ff3bbde14cef5947495e095ac06ec5cabc2d
muhammadwali0 <waleeraxa@gmail.com>
2026-02-12 22:32:01 +0500

    feat: add a program to implement membership testing

---
+++ 03-data-structures/membership.py
@@ -0,0 +1,8 @@
+## Program to test membership of different numbers for a set of first ten positive integers
+
+num = {i for i in range(1,11)}
+print(11 in num)
+print(5 in num)
+print(6 in num)
+print(1 in num)
+print(22 in num)
