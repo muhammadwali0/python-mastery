f44d07e54a6c34a53d51393ff6e63f786ea312b3
muhammadwali0 <waleeraxa@gmail.com>
2026-02-14 20:53:17 +0500

    feat: add a program to create nested dictionaries

---
+++ 03-data-structures/nestedDict.py
@@ -0,0 +1,8 @@
+## Program to create a nested dictionary and print it
+
+student = {
+    "name": "Ali",
+    "age": "19",
+    "grade": {"math": "A", "science": "B", "english": "C"},
+}
+print(student)
