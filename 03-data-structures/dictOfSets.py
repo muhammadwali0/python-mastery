2fbed44afd6492366bc95017b2a9648e4a0d5016
muhammadwali0 <waleeraxa@gmail.com>
2026-02-15 00:43:08 +0500

    feat: add a program to create a dictionary with sets as keys and integers as values

---
+++ 03-data-structures/dictOfSets.py
@@ -0,0 +1,9 @@
+## Program to create a dictionary of sets as keys and integers as values
+
+num = {
+    frozenset([1, 2, 3, 4, 5]): 1,
+    frozenset([6, 7, 8, 9, 10]): 2,
+    frozenset([11, 12, 13, 14, 15]): 3,
+}
+
+print(num)
