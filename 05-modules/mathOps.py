bed889a5af152ef4496614ef8c0f1a57585d94c2
muhammadwali0 <waleeraxa@gmail.com>
2026-02-27 03:01:42 +0500

    feat: add a program to utilize math module

---
+++ 05-modules/mathOps.py
@@ -0,0 +1,12 @@
+## Program to use math module to create GCD of two numbers and factorial of a number
+
+import math
+
+num1 = 30
+num2 = 90
+
+greatestCommonDivisor = math.gcd(num1, num2)
+factor = math.factorial(num1)
+
+print(greatestCommonDivisor)
+print(factor)
