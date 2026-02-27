c4ef0b9d136c88a0d082b4c71762106d5c046b89
muhammadwali0 <waleeraxa@gmail.com>
2026-02-28 02:36:14 +0500

    feat: create __init__ and call the modules in a file and use its' functions

---
+++ 05-modules/mypackage/__init__.py
@@ -0,0 +1,3 @@
+## Program to modify the package to include an `__init__.py` file that imports the functions from modules and write code to use these functions.
+
+
---
+++ 05-modules/package.py
@@ -0,0 +1,6 @@
+## Program to modify the package to include an `__init__.py` file that imports the functions from modules and write code to use these functions.
+
+from mypackage import module1, module2
+
+print(module1.sum(2, 8))
+print(module2.product(2, 8))
