35a25d78b358cd795733496f383601a66f8f4bfa
muhammadwali0 <waleeraxa@gmail.com>
2026-02-27 16:27:58 +0500

    feat: create a package with modules containing functions

---
+++ 05-modules/mypacakge/module1.py
@@ -0,0 +1,5 @@
+## Program to create a package with two modules, one should contain a function that adds two numbers, and second should contain a function that multiplies two numbers
+
+
+def sum(a=0, b=0):
+    return a + b
