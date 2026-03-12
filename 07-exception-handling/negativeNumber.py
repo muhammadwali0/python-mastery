0bbe96857eb5c1fed3a3488b1b9c64edd1238a91
muhammadwali0 <waleeraxa@gmail.com>
2026-03-12 13:00:36 +0500

    refactor: clean a program

--- 07-exception-handling/negativeNumber.py
+++ 07-exception-handling/negativeNumber.py
@@ -1,13 +1,13 @@
 ## Program to define a custom exception named `NegativeNumberError`. Write a function that raises this exception if a negative number is encountered in a list. Use try, except, and finally blocks to handle the custom exception and print an appropriate message.
 
-from typing import Any, List
+from typing import List
 
 
 class NegativeNumberError(Exception):
     pass
 
 
-def negativeNumChecker(items: List[Any]) -> None:
+def negativeNumChecker(items: List[int | float]) -> None:
     for item in items:
         if item < 0:
             raise NegativeNumberError("less than zero")
